97e7659041b9ce5c6cdc6ad9200acabed59bb695
MarBozzz <mbozzuto@libero.it>
2023-01-10 17:24:41 +0100

    [printed] results in page

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -2,6 +2,24 @@
 
 @section('content')
     <h1>
-        TRAINS
+        <div class="container">
+            <h1 class="text-center pb-5">Orari Treni</h1>
+
+            <div class="row text-center">
+                 @foreach ($trains as $train)
+                     <div class="col-3">
+                        <a href="#" class="card mb-5 pb-5">
+                            <div class="card-body">
+                            <h3 class="card-title py-5">Treno: {{ $train->company_name }}</h3>
+                            <p class="card-text">Numero: {{ $train->train_code }}</p>
+                            <p class="card-text">Proveniente da: {{ $train->departure_station }}</p>
+                            <p class="card-text">Diretto a: {{ $train->arrival_station }}</p>
+                            </div>
+                        </a>
+                     </div>
+
+                 @endforeach
+            </div>
+        </div>
     </h1>
 @endsection
